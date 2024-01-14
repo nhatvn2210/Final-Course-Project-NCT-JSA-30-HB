964a294a434360b3509857a3727c040d1d293346
nhatvn2210 <coiminhnhat2210@gmail.com>
2024-01-14 10:53:13 +0700

    Add product

--- src/JS/shop.js
+++ src/JS/shop.js
@@ -5,15 +5,57 @@ const searchInput = document.querySelector("#search-input");
 // Product list
 let productList = [
     {
+        id: "0001",
         imgSrc: "https://down-vn.img.susercontent.com/file/vn-11134207-7r98o-lp62u10kmamm32",
-        name: "Giỏ quà Tết 1",
+        name: "Giỏ quà Tết",
         rate: 5,
         price: 100000,
         description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, saepe? Aliquam commodi ex quaerat provident facilis, dicta ad iste assumenda consequuntur amet eveniet voluptatibus beatae accusamus officia tenetur voluptatum fugiat?",
     },
     {
-        imgSrc: "https://down-vn.img.susercontent.com/file/vn-11134207-7r98o-lp62u10kmamm32",
-        name: "Giỏ quà Tết 2",
+        id: "0002",
+        imgSrc: "https://down-vn.img.susercontent.com/file/vn-11134201-7r98o-lpc4v5mph10b1a",
+        name: "Áo dài tết nữ",
+        rate: 5,
+        price: 85000,
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, saepe? Aliquam commodi ex quaerat provident facilis, dicta ad iste assumenda consequuntur amet eveniet voluptatibus beatae accusamus officia tenetur voluptatum fugiat?",
+    },
+    {
+        id: "0003",
+        imgSrc: "https://down-vn.img.susercontent.com/file/vn-11134207-7r98o-lnvbtlxo33zhde",
+        name: "Áo dài tết nam",
+        rate: 5,
+        price: 120000,
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, saepe? Aliquam commodi ex quaerat provident facilis, dicta ad iste assumenda consequuntur amet eveniet voluptatibus beatae accusamus officia tenetur voluptatum fugiat?",
+    },
+    {
+        id: "0004",
+        imgSrc: "https://down-vn.img.susercontent.com/file/vn-11134207-7r98o-lprjrbna7sjb19",
+        name: "Mô hình bánh chưng",
+        rate: 4,
+        price: 90000,
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, saepe? Aliquam commodi ex quaerat provident facilis, dicta ad iste assumenda consequuntur amet eveniet voluptatibus beatae accusamus officia tenetur voluptatum fugiat?",
+    },
+    {
+        id: "0005",
+        imgSrc: "https://down-vn.img.susercontent.com/file/vn-11134207-7r98o-ll5yrtc8kp120e",
+        name: "Bánh chưng vuông",
+        rate: 4,
+        price: 45000,
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, saepe? Aliquam commodi ex quaerat provident facilis, dicta ad iste assumenda consequuntur amet eveniet voluptatibus beatae accusamus officia tenetur voluptatum fugiat?",
+    },
+    {
+        id: "0006",
+        imgSrc: "https://down-vn.img.susercontent.com/file/f0d6a7502c028f81ea0d0e889044f18d",
+        name: "Bánh tét ngũ sắc",
+        rate: 4,
+        price: 90000,
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, saepe? Aliquam commodi ex quaerat provident facilis, dicta ad iste assumenda consequuntur amet eveniet voluptatibus beatae accusamus officia tenetur voluptatum fugiat?",
+    },
+    {
+        id: "0007",
+        imgSrc: "https://down-vn.img.susercontent.com/file/vn-11134207-7r98o-loxipffaz0i628",
+        name: "Mô hình bánh tét",
         rate: 4,
         price: 90000,
         description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, saepe? Aliquam commodi ex quaerat provident facilis, dicta ad iste assumenda consequuntur amet eveniet voluptatibus beatae accusamus officia tenetur voluptatum fugiat?",
@@ -28,10 +70,10 @@ function renderProduct(selectedList) {
     let htmls = ``;
     // A loop to get data from selected list
     for (let i in selectedList) {
-        let imgURL = productList[i].imgSrc;
-        let productName = productList[i].name;
-        let productRate = productList[i].rate;
-        let productPrice = productList[i].price;
+        let imgURL = selectedList[i].imgSrc;
+        let productName = selectedList[i].name;
+        let productRate = selectedList[i].rate;
+        let productPrice = selectedList[i].price;
         htmls += `
         <li class="product-item">
             <div class="img" style="background-image: url(${imgURL}) ;"></div>
