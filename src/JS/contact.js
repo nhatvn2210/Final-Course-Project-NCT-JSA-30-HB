fd98e92255318014ff79ac8a2e2cf51797140897
nhatvn2210 <coiminhnhat2210@gmail.com>
2024-02-18 09:36:21 +0700

    Final Project

--- src/JS/contact.js
+++ src/JS/contact.js
@@ -1,13 +1,27 @@
 // DOM
 
 const submitForm = document.querySelector('.right-form');
+const subjectInput = document.querySelector('#subject');
+const messageInput = document.querySelector('#message');
+const currentSignInAccount = JSON.parse(localStorage.getItem('current-signin-account')) || '';
+console.log(currentSignInAccount);
 // Function
-function handleContact(event){
+function handleContact(event) {
+    // Prevent from reloading
     event.preventDefault();
+    // Compare
+    if (!currentSignInAccount) {
+        alert('Bạn phải đăng nhập trước đã!');
+        return ;
+    }
+    if (!subjectInput || !messageInput) {
+        alert("Bạn phải điền hết tất cả chỗ trống!");
+        return ;
+    }
     alert("Gửi thư thành công!");
 }
 // Main script
 
-submitForm.addEventListener('submit', function(event){
+submitForm.addEventListener('submit', function (event) {
     handleContact(event);
 })
